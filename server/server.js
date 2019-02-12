ee8c5e1759353ab0c4d8d3375fdb01c8dcd60d74
Vladimir Mezentsov <vmezentsov@InComm.com>
2019-02-12 08:54:14 -0500

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -12,13 +12,29 @@ var io = socketIO(server);
 io.on("connection", socket => {
   console.log("New user connected");
 
+  socket.emit("newMessage", {
+    from: "Admin",
+    text: "Welcome user one"
+  });
+
+  socket.broadcast.emit("newMessage", {
+    from: "Admin",
+    text: "New has joined the room",
+    createdAt: new Date().getTime()
+  });
+
   socket.on("createMessage", message => {
     console.log("createMessage ", message);
-    io.emit('newMessage', {
+    io.emit("newMessage", {
       from: message.from,
       text: message.text,
-      createdAt: new Date().getTime() 
-    })
+      createdAt: new Date().getTime()
+    });
+    // socket.broadcast.emit("newMessage", {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on("disconnect", () => {
