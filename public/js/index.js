1aaf3a91ef11f5d5400fc87351d9abdc0a22d890
Vladimir Mezentsov <vmezentsov@InComm.com>
2019-02-14 15:51:58 -0500

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -16,26 +16,30 @@ socket.on("newMessage", function(message) {
   $("#messages").append(li);
 });
 
-socket.on('newLocationMessage', function(message){
+socket.on("newLocationMessage", function(message) {
   var li = $("<li></li>");
-  var a = $("<a target='_blank'>My current location</a>")
+  var a = $("<a target='_blank'>My current location</a>");
 
-  li.text(`${message.from}: `)
-  a.attr('href', message.url)
-  li.append(a)
+  li.text(`${message.from}: `);
+  a.attr("href", message.url);
+  li.append(a);
   $("#messages").append(li);
-})
+});
 
 $("#message-form").on("submit", function(e) {
   e.preventDefault();
+
+  var messageTextBox = $("[name=message]");
 
   socket.emit(
     "createMessage",
     {
       from: "User",
-      text: $("[name=message]").val()
+      text: messageTextBox.val()
     },
-    function() {}
+    function() {
+      messageTextBox.val("");
+    }
   ); // end socket.on
 });
 
@@ -45,15 +49,18 @@ locationButton.on("click", function() {
     return alert("Geolocation not supported by your browser.");
   }
 
+  locationButton.attr("disabled", "disabled").text("Sending location...");
+
   navigator.geolocation.getCurrentPosition(
     function(position) {
-      console.log(position)
-      socket.emit('createLocationMessage', {
+      locationButton.removeAttr("disabled").text("Send location");
+      socket.emit("createLocationMessage", {
         latitude: position.coords.latitude,
         longitude: position.coords.longitude
-      })
+      });
     },
     function() {
+      locationButton.removeAttr("disabled").text("Send location");
       alert("Unable to fetch location");
     }
   );
